c483f09c1ade1338c2b0d21a22dfa4cf5e0aa554
Bilal <your_email@example.com>
2025-12-05 11:14:31 -0800

    Update

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -102,13 +102,30 @@ const AuthPage = () => {
 
                     <button
                         onClick={handleGoogleSignIn}
-                        className={`w-full flex items-center justify-center gap-3 px-4 py-3 bg-white 
+                        className={`w-full flex items-center justify-center gap-3 px-4 py-3 cursor-pointer bg-white 
                         text-gray-800 rounded-lg font-semibold transition mb-6 
-                       ${loading ? "opacity-50 cursor-not-allowed" : "hover:bg-gray-100"}`}
+                        ${loading ? "opacity-50 cursor-not-allowed" : "hover:bg-gray-100"}`}
                         disabled={loading}
                     >
+                        {/* Google SVG */}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+                            <path fill="#FFC107" d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8-6.627 
+        0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 
+        6.053 29.268 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"/>
+                            <path fill="#FF3D00" d="M6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 
+        12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 
+        4 16.318 4 9.656 8.337 6.306 14.691z"/>
+                            <path fill="#4CAF50" d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.182-5.238C29.211 
+        35.091 26.715 36 24 36c-5.202 0-9.598-3.317-11.254-7.946l-6.522 
+        5.025C9.63 39.556 16.335 44 24 44z"/>
+                            <path fill="#1976D2" d="M43.611 20.083H42V20H24v8h11.303c-.793 
+        2.239-2.231 4.166-4.076 5.57L37.409 38.808C41.014 
+        35.539 43.351 30.979 43.611 25.917z"/>
+                        </svg>
+
                         Continue with Google
                     </button>
+
 
                     <div className="flex items-center gap-4 mb-6">
                         <div className="flex-1 border-t border-white/20"></div>
@@ -189,14 +206,14 @@ const AuthPage = () => {
                         <button
                             type="submit"
                             disabled={loading}
-                            className={`w-full bg-green-600 px-6 py-3 rounded-lg text-white font-semibold transition 
+                            className={`cursor-pointer w-full bg-green-600 px-6 py-3 rounded-lg text-white font-semibold transition 
                             ${loading ? "opacity-50 cursor-not-allowed" : "hover:bg-green-700"}`}
                         >
                             {loading
                                 ? "Processing..."
                                 : isLogin
-                                ? "Login"
-                                : "Sign Up"}
+                                    ? "Login"
+                                    : "Sign Up"}
                         </button>
                     </form>
 
@@ -213,7 +230,7 @@ const AuthPage = () => {
                                         confirmPassword: "",
                                     });
                                 }}
-                                className="text-green-400 hover:underline"
+                                className="cursor-pointer text-green-400 hover:underline"
                             >
                                 {isLogin ? "Sign Up" : "Login"}
                             </button>
